12f3a1bd86944cde63ec860ce030fbec47162ce5
brpeanut <bresheske@gmail.com>
2017-10-16 09:17:18 -0500

    m

--- src/web.js
+++ src/web.js
@@ -1,6 +1,6 @@
 /* This file is to run the production version under nodejs. */
 var express = require('express');
 var app = express();
-app.use(express.static('./'));
+app.use(express.static(__dirname));
 app.listen(80);
 console.log('ng-gpioui listening on 80.');
